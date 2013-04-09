d4478b286bd1a4360a67803fd06c64ba1fe4e9b8
Steve Perkins <stereosteve@gmail.com>
2013-04-09 10:47:51 -0400

    load + store token in cookie

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var domify = require('domify')
   , agent = require('agent')
   , Dialog = require('dialog').Dialog
-
+  , cookie = require('cookie')
 
 var form = domify(require('./template'))[0];
 var loginModal = new Dialog();
 modalBody = loginModal.el.find('.body');
 modalBody.html(form);
+
+// try to load your existing token
+loadSessionCookie();
 
 form.onsubmit = function(e) {
   e.preventDefault();
@@ -17,8 +20,7 @@ form.onsubmit = function(e) {
   agent.post('/login', payload, function(resp) {
     if (resp.status === 200) {
       exports.token = resp.body.access_token
-      console.log("TOKEN!", exports.token)
-      loginModal.hide();
+      onToken();
     }
     else {
       console.log("login failed")
@@ -28,4 +30,24 @@ form.onsubmit = function(e) {
 
 exports.show = function() {
   loginModal.overlay().show()
+}
+
+function saveSessionCookie() {
+  var domain = location.hostname.match(/(\.[^.]+\.[^.]+)$/)[1];
+  cookie('indabaAccessToken', exports.token, {
+    maxage: 1000 * 60 * 60 * 24 * 365, // about a year
+    path: '/',
+    domain: domain
+  });
+}
+
+function loadSessionCookie() {
+  exports.token = cookie('indabaAccessToken') || null;
+  if (exports.token) onToken();
+}
+
+function onToken() {
+  saveSessionCookie();
+  loginModal.hide();
+  console.log("onToken!", exports.token)
 }
